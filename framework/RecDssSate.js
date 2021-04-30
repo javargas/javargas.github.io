b1fb15445abe1a60203f81372b3f446f779a3f14
John Vargas <john.vargas@wizeline.com>
2021-04-29 19:54:39 -0500

    RecDssState with data storage

--- framework/RecDssSate.js
+++ framework/RecDssSate.js
@@ -1,46 +1,16 @@
 
-
-/* ============================
-// HO TO USE IN MODULES
-==============================
-/* * /
-
-//  pass to nex step in the process
-recDssState.nextStep();
-
-// register an observer
-recDssState.subscribe((step) => {
-    // handler code here
-});
-
-// add a validation function
-recDssState.addValidator(step, validateFunction );
-
-// Save data in global object
-recDssState.data[data_variable] = { 
-    vbl1 : data1,
-    vbl2 : data2
-}; 
-
-/* */
-
-/* ==========================
-// Framework global object
-===========================*/
-
 class RecDssState {
   constructor() {
     this.activeStep = ${initial_step}; // current step in the checkout process
     this.observers = []; // observers modules
-    this.data = []; // List of data objects for each module
     this.validators = []; // Validator functions per step
 
-    this.states = ['email', 'login', 'payment', 'summary'];
+    // Get the existing data
+    let existing = sessionStorage.getItem("recDssState");
 
-    if(sessionStorage.recDssState)
-    {
-     this.data = JSON.parse(sessionStorage.getItem('recDssState'));
-    }
+    // If no existing data, use the value by itself
+	  // Otherwise, add the new value to it
+    this.data = existing ? JSON.parse(existing) : {};
   }
 
   subscribe(f) {
@@ -66,29 +36,30 @@ class RecDssState {
   nextStep() {
     let canContinue = true;
 
-    this.validators[this.activeStep]
-    .forEach(
-        (validator) => {
-            if (!validator()) {
-                canContinue = false;
-            }
-        }
-    );
+    if (typeof this.validators[this.activeStep] != "undefined") {
+      this.validators[this.activeStep]
+      .forEach(
+          (validator) => {
+              if (!validator()) {
+                  canContinue = false;
+              }
+          }
+      );
+    }
 
     if (canContinue) {
         console.log("continue to the next step: ", this.data);
-        this.notify(++this.activeStep);
-
         this.saveData();
+        this.notify(++this.activeStep);
     }
 
     return canContinue;
   }
 
   saveData() {
-    sessionStorage.setItem('recDssState', JSON.stringify(this.data));
+    console.log("saving data ", this.data);
+    sessionStorage.setItem("recDssState", JSON.stringify(this.data));
   }
 }
 
 var recDssState = new RecDssState();
-
